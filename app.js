75027652c99b7df894989588f6cbb10f882b6d4f
Adewoye Adegoke <69061153+Degoke@users.noreply.github.com>
2022-04-15 00:56:13 +0100

    solved error

--- app.js
+++ app.js
@@ -9,49 +9,53 @@ const app = new App({
 app.event('app_home_opened', async({ event, client, context}) => {
     try {
         await client.views.publish({
-            "attachments": [
-                {
-                    "color": "#f2c744",
-                    "blocks": [
-                        {
-                            "type": "section",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "This is a plain text section block.",
-                                "emoji": true
-                            }
-                        },
-                        {
-                            "type": "section",
-                            "text": {
-                                "type": "mrkdwn",
-                                "text": "This is a mrkdwn section block :ghost: *this is bold*, and ~this is crossed out~, and <https://google.com|this is a link>"
-                            }
-                        },
-                        {
-                            "type": "section",
-                            "text": {
-                                "type": "mrkdwn",
-                                "text": "This is a section block with a button."
-                            },
-                            "accessory": {
-                                "type": "button",
+            user_id: event.user,
+            view: {
+                "attachments": [
+                    {
+                        "color": "#f2c744",
+                        "blocks": [
+                            {
+                                "type": "section",
                                 "text": {
                                     "type": "plain_text",
-                                    "text": "Click Me",
+                                    "text": "This is a plain text section block.",
                                     "emoji": true
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "This is a mrkdwn section block :ghost: *this is bold*, and ~this is crossed out~, and <https://google.com|this is a link>"
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "This is a section block with a button."
                                 },
-                                "value": "click_me_123",
-                                "action_id": "button-action"
+                                "accessory": {
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "Click Me",
+                                        "emoji": true
+                                    },
+                                    "value": "click_me_123",
+                                    "action_id": "button-action"
+                                }
+                            },
+                            {
+                                "type": "divider"
                             }
-                        },
-                        {
-                            "type": "divider"
-                        }
-                    ]
-                }
-            ]
-        })
+                        ]
+                    }
+                ]
+            }
+            }
+            )
     }
     catch(error) {
         console.log(error)
